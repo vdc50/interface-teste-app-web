3ec0933484c61fff842af0775cd32f870a74430b
Vinicius Dantas <viniemmovimento@gmail.com>
2023-06-04 20:37:36 -0300

    primeira logica com js, carrosel no main

---
+++ logica.js
@@ -0,0 +1,36 @@
+/* VARIAVEIS */
+const controls =
+    document.querySelectorAll('.control');
+let currentItem = 0
+const items =
+    document.querySelectorAll('.item');
+const maxItems = items.length;
+
+/* EVENTOS */
+controls.forEach(control => {
+    control.addEventListener('click', (e) => {
+        isLeft =
+            control.classList.contains("arrow-left");
+
+        if (isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+
+        if (currentItem >= maxItems) {
+            currentItem = 0;
+        }
+        if (currentItem < 0) {
+            currentItem - maxItems - 1;
+        }
+        items.forEach((item) => item.classList.remove("current-item"));
+        
+        items[currentItem].scrollIntoView({
+            behavior: "smooth",
+            inline: "center"
+        });
+        
+        items[currentItem].classList.add("current-item");
+    });
+  });
